776cd1f35ceab9ac997cb0bac4b6e9a1be37eb9f
Clément <topy72.mine@gmail.com>
2021-02-09 14:32:20 +0100

    Création du runner et génération de terrain

---
+++ Assets/Scripts/C#/Range.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using UnityEngine;
+
+[Serializable]
+public class Range
+{
+    public float min;
+    public float max;
+
+    public float range
+    {
+        get => this.max - this.min;
+    }
+    
+    public Range(float min, float max)
+    {
+        this.max = Mathf.Max(min, max);
+        this.min = Mathf.Min(min, max);
+    }
+
+    public float Clamp(float value)
+    {
+        return Mathf.Clamp(value, min, max);
+    }
+}
--- Assets/Scripts/C#/TerrainGenerator.cs
+++ Assets/Scripts/C#/TerrainGenerator.cs
@@ -1,78 +1,66 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.U2D;
+using Random = UnityEngine.Random;
 
+[ExecuteInEditMode]
 public class TerrainGenerator : MonoBehaviour
 {
     public int numberOfPoints;
     public int heightOffset = 0;
 
-    private LineRenderer _lr;
-    private PolygonCollider2D _pc;
     private SpriteShapeController _ss;
 
-    public float terrainOffset = 0;
+    public Vector2 terrainOffset = Vector2.zero;
 
     void Start()
     {
-        _lr = GetComponent<LineRenderer>();
-        _pc = GetComponent<PolygonCollider2D>();
-
-        Debug.Assert(_lr);
-
-        terrainOffset += Random.Range(-100000, 100000);
+        _ss = GetComponent<SpriteShapeController>();
+        
+        terrainOffset = new Vector2(Random.Range(-100000, 100000), Random.Range(-100000, 100000));
         DrawTerrain();
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKey(KeyCode.LeftArrow))
-        {
-            terrainOffset -= Time.deltaTime * 5;
-            DrawTerrain();
-        }
-
-        if (Input.GetKey(KeyCode.RightArrow))
-        {
-            terrainOffset += Time.deltaTime * 5;
-            DrawTerrain();
-        }
+        if (Application.isEditor) return;
     }
 
-    void DrawTerrain()
+    public void DrawTerrain()
     {
-        // Reset points
-        _lr.SetPositions(new Vector3[0] { });
-        _lr.positionCount = numberOfPoints;
-
+        // Get Camera Bounds and set bounds variables
         var bounds = GetCameraBounds();
-
-        _pc.SetPath(0, new Vector2[0] { });
         var end = new Vector2(bounds.xMax, bounds.yMin);
-        var pointsArr = new List<Vector2>() { bounds.min };
-
-
         var maxHeight = bounds.height / 2;
         var range = (bounds.width / numberOfPoints);
 
+        // Clear Sprite Shape and set first position
+        var spline = _ss.spline;
+        spline.Clear();
+        spline.InsertPointAt(0, bounds.min - Vector2.one);
+        var lastIndex = 0;
+
+        // Create the terrain
         for (int i = 0; i < numberOfPoints; i++)
         {
+            // Calculate X, generate value and create Y based on that
             var x = range * i;
-            var value = Mathf.PerlinNoise((x + terrainOffset) / 10, 0) + 0.2f; // * Mathf.PerlinNoise((x + terrainOffset)/3, 0);
+            var value = Mathf.PerlinNoise((x + terrainOffset.x) / 10, terrainOffset.y) + 0.2f; // * Mathf.PerlinNoise((x + terrainOffset)/3, 0);
             var newPoint = new Vector3(x - bounds.width / 2, (value * maxHeight - bounds.height / 2) + heightOffset, -2);
-            _lr.SetPosition(i, newPoint);
-            if (i % 10 == 0)
-            {
-                pointsArr.Add(newPoint);
-            }
+            newPoint.y += .1f;
+
+            // Add point to the Sprite Shape
+            spline.InsertPointAt(i+1, newPoint);
+            spline.SetTangentMode(i+1, ShapeTangentMode.Broken);
+            lastIndex = i+1;
         }
 
-        
-        pointsArr.Add(end);
-
-        _pc.SetPath(0, pointsArr);
+        // Add last point to the Sprite Shape
+        spline.InsertPointAt(lastIndex+1, end - new Vector2(-1, 1));
+        spline.SetTangentMode(lastIndex+1, ShapeTangentMode.Broken);
     }
 
     private Rect GetCameraBounds()
